ec0e37af5f72c96f5b19a63b23bb3f15216040c7
Daniel Jarski <danieljarski@gmail.com>
2022-12-15 16:52:28 -0600

    Add (barely-functioning) day 12

---
+++ Challenges/2022/HillClimbingAlgorithm.cs
@@ -0,0 +1,153 @@
+using System.Drawing;
+using AdventOfCode.Common;
+
+namespace AdventOfCode.Challenges;
+
+[Challenge("Hill Climbing Algorithm", 2022, 12)]
+public sealed class HillClimbingAlgorithm : AdventOfCodeChallenge
+{
+    private Graph<HeightPoint> _map = null!;
+    
+    public override void LoadData()
+    {
+        var split = _input.Split('\n', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        var arr = new HeightPoint[split.Length, split[0].Length];
+        
+        for (var x = 0; x < split.Length; x++)
+        {
+            var line = split[x];
+            for (var y = 0; y < line.Length; y++)
+            {
+                arr[x, y] = new HeightPoint(line[y], new Point(x, y));
+            }
+        }
+
+        _map = new Graph<HeightPoint>(arr);
+    }
+
+    public override string SolvePart1()
+    {
+        var s = _map.Single(x => x.Height == 'S');
+        var e = _map.Single(x => x.Height == 'E');
+
+        var list = new HashSet<Path>();
+
+        var path = new Path(new[] {s}.ToList());
+
+        _map.Print(true);
+        
+        Traverse(path);
+
+        foreach (var p in list.Where(x => x.VisitedPoints[^1].Equals(e)))
+        {
+            _map.Print(true, p.VisitedPoints.Select(x => (x.Location, ConsoleColor.Green)).ToArray());
+            Console.WriteLine($"Length: {p.Length}");
+            Console.ReadKey();
+        }
+
+        return $"Shortest path is {list.Where(x => x.VisitedPoints[^1].Equals(e)).MinBy(x => x.Length).Length} steps.";
+        
+        // list is a list of paths, which starts with one path, starting from S
+        void Traverse(Path currentPath)
+        {
+            var current = currentPath.VisitedPoints[^1];
+            
+            var neighbors = _map.GetCartesianNeighbors(current, p => current.CanTravelTo(p.Height));
+
+            foreach (var neighbor in neighbors)
+            {
+                var p = new Path(currentPath.VisitedPoints.Append(neighbor).ToList());
+                
+                if (neighbor.Equals(s)) // special case. don't go right back to start idiot
+                    continue;
+
+                if (neighbor.IsTheTop)
+                {
+                    return;
+                }
+
+                // problem line #1
+
+                var currentPathLength = p.Length;
+                if (list.Any(x =>
+                    {
+                        var length = x.StepsTo(neighbor);
+                        // is there a shorter path to this neighbor?
+                        return length > 0 && length <= currentPathLength;
+                    }))
+                {
+                    continue;
+                }
+
+                // problem line #2?
+                // currentPath.VisitedPoints.Add(neighbor);
+
+                list.Add(p);
+                
+                //Console.SetCursorPosition(position.Left, position.Top);
+                //Console.WriteLine(list.Count);
+                //var tempPosition = Console.GetCursorPosition();
+                //Console.WriteLine("\n\n\n\n\n\n\n\n\n\n");
+                //Console.SetCursorPosition(tempPosition.Left, tempPosition.Top);
+                //Console.Write(currentPath);
+
+                _map.PrintFast(currentPath.VisitedPoints.Select(x => (x.Location, ConsoleColor.Red)).ToArray());
+
+                Traverse(p);
+            }
+        }
+    }
+
+    public override string SolvePart2()
+    {
+        return string.Empty;
+    }
+
+    private readonly record struct Path(List<HeightPoint> VisitedPoints)
+    {
+        //public List<HeightPoint> VisitedPoints { get; } = new();
+
+        public int Length => VisitedPoints.Distinct().Count() - 1;// VisitedPoints.Count - 1; // don't include the starting point
+
+        public int StepsTo(HeightPoint point)
+         => VisitedPoints.IndexOf(point)/* + 1*/; // no + 1, see Length
+        
+        public bool Equals(Path other)
+            => other.VisitedPoints.SequenceEqual(VisitedPoints);
+
+        public override int GetHashCode()
+            => VisitedPoints.GetHashCode();
+
+        public override string ToString()
+            => new(VisitedPoints.Select(x => x.Height).ToArray());
+    }
+
+    private readonly record struct HeightPoint(char Height, Point Location)
+    {
+        public bool IsTheTop => Height == 'E';
+        
+        // can travel to a point at most 1 height greater than the current point,
+        // but can travel to a point of any height LESS than the current point
+        public bool CanTravelTo(char otherHeight)
+        {
+            if (otherHeight == 'E')
+                return Height == 'z';
+
+            if (otherHeight is 'a' or 'b' && Height == 'S')
+                return true;
+
+            return otherHeight - Height <= 1;
+        }
+
+        public bool Equals(HeightPoint? other)
+            => other?.Location.Equals(Location) == true;
+
+        public override int GetHashCode()
+            => Location.GetHashCode();
+
+        public override string ToString()
+            => Height.ToString();
+
+        public static implicit operator Point(HeightPoint p) => p.Location;
+    }
+}
--- Common/Graph.cs
+++ Common/Graph.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections;
+using System.Drawing;
+using System.Text;
 
 namespace AdventOfCode.Common;
 
@@ -46,10 +48,119 @@ public class Graph<T> : IEnumerable<T>
         }
     }
 
+    public T[] GetCartesianNeighbors(Point point, Predicate<T>? predicate = null)
+        => GetCartesianNeighbors(point.X, point.Y, predicate);
+
+    public T[] GetCartesianNeighbors(int x, int y, Predicate<T>? predicate = null)
+    {
+        var list = new List<T>();
+        
+        if (y > 0)
+        {
+            var value = _arr[x, y - 1];
+            if (predicate?.Invoke(value) != false)
+                list.Add(value);
+        }
+
+        if (y < _arr.GetLength(1) - 1)
+        {
+            var value = _arr[x, y + 1];
+            if (predicate?.Invoke(value) != false)
+                list.Add(value);
+        }
+
+        if (x > 0)
+        {
+            var value = _arr[x - 1, y];
+            if (predicate?.Invoke(value) != false)
+                list.Add(value);
+        }
+
+        if (x < _arr.GetLength(0) - 1)
+        {
+            var value = _arr[x + 1, y];
+            if (predicate?.Invoke(value) != false)
+                list.Add(value);
+        }
+
+        return list.ToArray();
+    }
+
     public int TotalLength => _arr.Length;
 
     public int GetLength(GraphDimension dimension)
         => _arr.GetLength((int) dimension);
+
+    public void Print(bool writeOver, params (Point Point, ConsoleColor Color)[] highlightedPoints)
+    {
+        if (writeOver)
+        {
+            //Console.Clear();
+            Console.SetCursorPosition(0, 0);
+
+            var builder = new StringBuilder();
+            for (var x = 0; x < _arr.GetLength(0); x++)
+            {
+                for (var y = 0; y < _arr.GetLength(1); y++)
+                {
+                    var element = _arr[x, y];
+                    var location = new Point(x, y);
+
+                    var found = false;
+                    foreach (var (point, color) in highlightedPoints)
+                    {
+                        if (point != location)
+                            continue;
+                        
+                        Console.Write(builder);
+                        Console.ForegroundColor = color;
+                        Console.Write(element);
+                        Console.ResetColor();
+                        builder.Clear();
+                        found = true;
+                    }
+                    
+                    if (!found)
+                        builder.Append(_arr[x, y]);
+                }
+
+                builder.AppendLine();
+            }
+        
+            Console.WriteLine(builder.ToString());
+        }
+
+        /*
+        foreach (var (point, color) in highlightedPoints)
+        {
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(point.Y, point.X);
+            Console.Write(_arr[point.X, point.Y]);
+            Console.ResetColor();
+        }
+        
+        Console.SetCursorPosition(0, _arr.GetLength(0) + 1);
+        */
+
+        //Console.ReadLine();
+    }
+    
+    // does not un-color already colored characters
+    public void PrintFast(params (Point Point, ConsoleColor Color)[] highlightedPoints)
+    {
+
+        foreach (var (point, color) in highlightedPoints)
+        {
+            Console.SetCursorPosition(point.Y, point.X);
+            Console.ForegroundColor = color;
+            Console.Write(_arr[point.X, point.Y]);
+            Console.ResetColor();
+        }
+       
+        Console.SetCursorPosition(0, _arr.GetLength(0) + 1);
+
+        //Console.ReadLine();
+    }
 
     public IEnumerator<T> GetEnumerator() => ToEnumerable().GetEnumerator();
 
@@ -60,4 +171,8 @@ public class Graph<T> : IEnumerable<T>
         foreach (var item in _arr)
             yield return item;
     }
+
+    public static implicit operator T[,](Graph<T> graph) => graph._arr;
+
+    public static implicit operator Graph<T>(T[,] arr) => new(arr);
 }
